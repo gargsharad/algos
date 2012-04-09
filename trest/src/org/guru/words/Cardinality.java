a3a1d03ed89bbb0959192644d87666f1cc88d042
Guruprasad GV <guru.gv@gmail.com>
2012-04-09 19:17:44 +0530

    updates

--- trest/src/org/guru/words/Cardinality.java
+++ trest/src/org/guru/words/Cardinality.java
@@ -1,16 +1,30 @@
 package org.guru.words;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 public class Cardinality {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		WordMap mp = new WordMap();
 		mp.addWord("guru".toCharArray());
 		mp.addWord("prasad".toCharArray());
 		mp.addWord("prasad".toCharArray());
 		mp.addWord("guru".toCharArray());
 		mp.addWord("gu".toCharArray());
+
+		BufferedReader reader = new BufferedReader(new FileReader(
+				"e:/gyan/shakespeare.txt"));
+		for (String line = reader.readLine(); line != null; line = reader
+				.readLine()) {
+			String[] words = line.trim().split(" ");
+			for (int i = 0; i < words.length; i++) {
+				mp.addWord(words[i].trim().toCharArray());
+			}
+
+		}
 		System.out.println(":" + mp.cardinality);
 		System.out.println("nodes " + mp.nodes);
 	}
@@ -22,7 +36,9 @@ public class Cardinality {
 		int nodes = 0;
 
 		public void addWord(char[] wordArr) {
-
+			if (wordArr.length == 0) {
+				return;
+			}
 			CharNode current = root;
 			for (int i = 0; i < wordArr.length; i++) {
 
@@ -30,7 +46,7 @@ public class Cardinality {
 					current = current.children.get(wordArr[i]);
 					if (i == wordArr.length - 1 && !current.eow) {
 						current.eow = true;
-						cardinality ++;
+						cardinality++;
 					}
 				} else {
 					CharNode ch = new CharNode(wordArr[i]);
